b7ab0ededceab73f0544b56e2c0c8e06832ef6d8
Clayman <Clayman@DESKTOP-649SK3N>
2021-05-20 18:01:14 +0300

    DataTypes day 2

--- DataTypes/DataTypes/main.cpp
+++ DataTypes/DataTypes/main.cpp
@@ -4,7 +4,29 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout << sizeof(int) << endl;
-	cout << INT_MIN << "\t" << INT_MAX << endl;
-	cout << 0 << "\t\t" << UINT_MAX << endl;
+	//cout << sizeof(int) << endl;
+	//cout << INT_MIN << "\t" << INT_MAX << endl;
+	//cout << 0 << "\t\t" << UINT_MAX << endl;
+	//cout << sizeof(float) << endl;
+	//cout << FLT_MIN << "\t" << FLT_MAX << endl;
+
+	int speed = 0;
+	const int MAX_SPEED = 250;
+	cout << sizeof(250) << endl;
+	cout << typeid(250u).name() << endl;
+	//http://cplusplus.com/doc/tutorial/constants/
+	
+	double Price;	//Объявление переменной Price
+	Price = 0;	//Инициализация переменной Price присвоением.
+	//Проинициализировать
+	//Инициализация - это присвоение начального значения.
+	cout << Price << endl;	//Использование (чтение) переменной Price
+	int a;
+	int t;
+	double price_of_coffee;
+	cout << "Сколько стоит чашка кофе: "; 
+	cin >> price_of_coffee;	//Инициализация вводом с клавиатуры
+	cout << price_of_coffee << endl;
+	price_of_coffee;
+	int number_of_cups;
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,7 +1,10 @@
 https://github.com/okovtun/BD_122.git
 https://www.youtube.com/watch?v=dKTDtpYuCPA&list=PLeqyOOqxeiIPJFPkv6UtPmvC9oMjaKCuO&index=1
 
-TODO:
+TOREAD:
+https://www.softelectro.ru/ieee754.html
+
+DONE:
 Используя вывод на экран и Escape-последовательности
 вывести в окно консоль стишок "В лесу родилась Ёлочка".
 В MyStat прислать *.cpp-файл.
