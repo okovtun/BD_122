78f930a0f124f4629db60daf455919ee05ce1f1b
Clayman <Clayman@DESKTOP-DM8E77U>
2021-07-01 15:43:59 +0300

    ShiftLeft DONE.

---
+++ Arrays/Shift/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int n = 10;
+	int arr[n] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	//Вывод исходного массива:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	int number_of_shifts;	//Количество сдвигов
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		//Сдвиг массива влево на 1 элемент:
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			//arr[i] = arr[i + 1];	//в текущий элемент помещаем значение следующего элемента.
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+	}
+
+	//Вывод сдвинутого массива:
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
