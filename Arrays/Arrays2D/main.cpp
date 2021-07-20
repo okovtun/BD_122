317dbbda39e685cf66e5682a379057f7980a64df
Clayman <Clayman@DESKTOP-DM8E77U>
2021-07-20 17:33:55 +0300

    2D sorting

--- Arrays/Arrays2D/main.cpp
+++ Arrays/Arrays2D/main.cpp
@@ -8,7 +8,7 @@ void main()
 	setlocale(LC_ALL, "Russian");
 	const int ROWS = 8;
 	const int COLS = 5;
-	int arr[ROWS][COLS] = 
+	int arr[ROWS][COLS] =
 	{
 		{3,		5,	8},
 		{13,	21,	34},
@@ -18,8 +18,52 @@ void main()
 	{
 		for (int j = 0; j < COLS; j++)
 		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+	//Вывод исходного массива на экран:
+	cout << "Исходный массив:\n";
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
 			cout << arr[i][j] << tab;
 		}
 		cout << endl;
 	}
+
+	//Сортировка двумерного массива:
+	int iterations = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			for (int k = i; k < ROWS; k++)
+			{
+				for (int l = k == i ? j + 1 : 0; l < COLS; l++)
+				{
+					if (arr[k][l] < arr[i][j])
+					{
+						int buffer = arr[i][j];
+						arr[i][j] = arr[k][l];
+						arr[k][l] = buffer;
+					}
+					iterations++;
+				}
+			}
+		}
+	}
+	cout << "Массив отсортирован за " << iterations << " итераций\n";
+
+	//Вывод отсортированного массива на экран:
+	cout << "Отсортированный массив:\n";
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+
 }
