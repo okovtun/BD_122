2b7e610525eaf2e2560f16128651766a7030801d
Clayman <Clayman@DESKTOP-DM8E77U>
2021-09-15 17:09:48 +0300

    DynamicMemory day 1

---
+++ Pointers/DynamicArrays/main.cpp
@@ -0,0 +1,40 @@
+﻿//DynamicArrays
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	//Stack
+	setlocale(LC_ALL, "");
+	int n;	//Размер массива
+	cout << "Введите размер массива: "; cin >> n;
+	int* arr = new int[n];//Heap
+
+	//Обращаться к элементам массива можно
+	FillRand(arr, n);
+	Print(arr, n);
+
+	delete[] arr;
+}
+
+void FillRand(int arr[], const int n)
+{
+
+	for (int i = 0; i < n; i++)
+	{
+		//Используя арифметику указателей и оператор разыменования:
+		*(arr + i) = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		//или же, через оператор индексирования[]:
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
