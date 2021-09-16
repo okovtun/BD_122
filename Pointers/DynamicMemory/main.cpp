690e02681f33d04c9c692b7c023101feb9f133d9
Clayman <Clayman@DESKTOP-DM8E77U>
2021-09-16 15:59:50 +0300

    DynamicMemory day 2

---
+++ Pointers/DynamicMemory/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+
+}
+
+void FillRand(int arr[], const int n)
+{
+	
+}
+void Print(int arr[], const int n)
+{
+
+}
